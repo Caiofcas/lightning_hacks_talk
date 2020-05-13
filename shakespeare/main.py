9f94bb24a2b5179c5fbc4d6b8f1a1ddf80372915
Caio <caiofcastro@usp.br>
2020-05-13 16:30:09 -0300

    refactoring

--- shakespeare/main.py
+++ shakespeare/main.py
@@ -4,62 +4,100 @@ import numpy as np
 import os
 import time
 
+
 # https://www.tensorflow.org/tutorials/text/text_generation
 
-DEBUG = True
+DEBUG = False
 
-path_to_file = tf.keras.utils.get_file(
-    'shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
 
-# Read, then decode for py2 compat.
-text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+def importText(DEBUG=False):
+    path_to_file = tf.keras.utils.get_file(
+        'shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
 
-# length of text is the number of characters in it
-if DEBUG:
-    print('Length of text: {} characters'.format(len(text)))
+    # Read, then decode for py2 compat.
+    text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
 
-# Take a look at the first 250 characters in text
-if DEBUG:
-    print("First 250 characters:\n")
-    print(text[:250], '\n')
+    # length of text is the number of characters in it
+    if DEBUG:
+        print('Length of text: {} characters'.format(len(text)))
 
-# The unique characters in the file
-vocab = sorted(set(text))
+    # Take a look at the first 250 characters in text
+    if DEBUG:
+        print("First 250 characters:\n")
+        print(text[:250], '\n')
 
-if DEBUG:
-    print('{} unique characters'.format(len(vocab)))
+    # The unique characters in the file
+    vocab = sorted(set(text))
 
-# Creating a mapping from unique characters to indices
-char2idx = {u: i for i, u in enumerate(vocab)}
-idx2char = np.array(vocab)
+    if DEBUG:
+        print('{} unique characters'.format(len(vocab)))
 
-text_as_int = np.array([char2idx[c] for c in text])
+    # Creating a mapping from unique characters to indices
+    char2idx = {u: i for i, u in enumerate(vocab)}
+    idx2char = np.array(vocab)
 
-if DEBUG:
-    print('{')
-    for char, _ in zip(char2idx, range(20)):
-        print('  {:4s}: {:3d},'.format(repr(char), char2idx[char]))
-    print('  ...\n}')
+    text_as_int = np.array([char2idx[c] for c in text])
 
-if DEBUG:
-    # Show how the first 13 characters from the text are mapped to integers
-    print(
-        '{} ---- characters mapped to int ---- > {}'.format(repr(text[:13]), text_as_int[:13]))
+    if DEBUG:
+        print('{')
+        for char, _ in zip(char2idx, range(20)):
+            print('  {:4s}: {:3d},'.format(repr(char), char2idx[char]))
+        print('  ...\n}')
 
-# The maximum length sentence we want for a single input in characters
-seq_length = 100
-examples_per_epoch = len(text)//(seq_length+1)
+    if DEBUG:
+        # Show how the first 13 characters from the text are mapped to integers
+        print(
+            '{} ---- characters mapped to int ---- > {}'.format(repr(text[:13]), text_as_int[:13]))
 
-# Create training examples / targets
-char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+    return text, vocab, char2idx, idx2char, text_as_int
 
-for i in char_dataset.take(5):
-    print(idx2char[i.numpy()])
 
-sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+def getDataset(text, idx2char, text_as_int, seq_length=100, DEBUG=False, BATCH_SIZE=64, BUFFER_SIZE=10000):
+    # The maximum length sentence we want for a single input in characters
+    examples_per_epoch = len(text)//(seq_length+1)
 
-for item in sequences.take(5):
-    print(repr(''.join(idx2char[item.numpy()])))
+    # Create training examples / targets
+    char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+    if DEBUG:
+        for i in char_dataset.take(5):
+            print(idx2char[i.numpy()])
+
+    sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+
+    if DEBUG:
+        for item in sequences.take(5):
+            print(repr(''.join(idx2char[item.numpy()])))
+
+    dataset = sequences.map(split_input_target)
+
+    if DEBUG:
+        for input_example, target_example in dataset.take(1):
+            print('Input data: ', repr(
+                ''.join(idx2char[input_example.numpy()])))
+            print('Target data:', repr(
+                ''.join(idx2char[target_example.numpy()])))
+
+    if DEBUG:
+        for i, (input_idx, target_idx) in enumerate(zip(input_example[:5], target_example[:5])):
+            print("Step {:4d}".format(i))
+            print("  input: {} ({:s})".format(
+                input_idx, repr(idx2char[input_idx])))
+            print("  expected output: {} ({:s})".format(
+                target_idx, repr(idx2char[target_idx])))
+
+    # Buffer size to shuffle the dataset
+    # (TF data is designed to work with possibly infinite sequences,
+    # so it doesn't attempt to shuffle the entire sequence in memory. Instead,
+    # it maintains a buffer in which it shuffles elements).
+
+    dataset = dataset.shuffle(BUFFER_SIZE).batch(
+        BATCH_SIZE, drop_remainder=True)
+
+    if DEBUG:
+        print(dataset)
+
+    return dataset
 
 
 def split_input_target(chunk):
@@ -68,42 +106,7 @@ def split_input_target(chunk):
     return input_text, target_text
 
 
-dataset = sequences.map(split_input_target)
-
-for input_example, target_example in dataset.take(1):
-    print('Input data: ', repr(''.join(idx2char[input_example.numpy()])))
-    print('Target data:', repr(''.join(idx2char[target_example.numpy()])))
-
-for i, (input_idx, target_idx) in enumerate(zip(input_example[:5], target_example[:5])):
-    print("Step {:4d}".format(i))
-    print("  input: {} ({:s})".format(input_idx, repr(idx2char[input_idx])))
-    print("  expected output: {} ({:s})".format(
-        target_idx, repr(idx2char[target_idx])))
-
-# Batch size
-BATCH_SIZE = 64
-
-# Buffer size to shuffle the dataset
-# (TF data is designed to work with possibly infinite sequences,
-# so it doesn't attempt to shuffle the entire sequence in memory. Instead,
-# it maintains a buffer in which it shuffles elements).
-BUFFER_SIZE = 10000
-
-dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
-
-print(dataset)
-
-# Length of the vocabulary in chars
-vocab_size = len(vocab)
-
-# The embedding dimension
-embedding_dim = 256
-
-# Number of RNN units
-rnn_units = 1024
-
-
-def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+def build_model(vocab_size, embedding_dim=256, rnn_units=1024, batch_size=64):
     model = tf.keras.Sequential([
         tf.keras.layers.Embedding(vocab_size, embedding_dim,
                                   batch_input_shape=[batch_size, None]),
@@ -116,59 +119,8 @@ def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
     return model
 
 
-model = build_model(
-    vocab_size=len(vocab),
-    embedding_dim=embedding_dim,
-    rnn_units=rnn_units,
-    batch_size=BATCH_SIZE)
-
-for input_example_batch, target_example_batch in dataset.take(1):
-    example_batch_predictions = model(input_example_batch)
-    print(example_batch_predictions.shape,
-          "# (batch_size, sequence_length, vocab_size)")
-
-sampled_indices = tf.random.categorical(
-    example_batch_predictions[0], num_samples=1)
-sampled_indices = tf.squeeze(sampled_indices, axis=-1).numpy()
-
-print(sampled_indices)
-
-print("Input: \n", repr("".join(idx2char[input_example_batch[0]])))
-print()
-print("Next Char Predictions: \n", repr("".join(idx2char[sampled_indices])))
-
-
 def loss(labels, logits):
     return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
-
-
-example_batch_loss = loss(target_example_batch, example_batch_predictions)
-print("Prediction shape: ", example_batch_predictions.shape,
-      " # (batch_size, sequence_length, vocab_size)")
-print("scalar_loss:      ", example_batch_loss.numpy().mean())
-
-model.compile(optimizer='adam', loss=loss)
-
-# Directory where the checkpoints will be saved
-checkpoint_dir = './training_checkpoints'
-# Name of the checkpoint files
-checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
-
-checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpoint_prefix,
-    save_weights_only=True)
-
-EPOCHS = 10
-history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
-
-tf.train.latest_checkpoint(checkpoint_dir)
-model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
-
-model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
-
-model.build(tf.TensorShape([1, None]))
-
-model.summary()
 
 
 def generate_text(model, start_string):
@@ -210,4 +162,64 @@ def generate_text(model, start_string):
     return (start_string + ''.join(text_generated))
 
 
-print(generate_text(model, start_string=u"ROMEO: "))
+def main():
+    text, vocab, char2idx, idx2char, text_as_int = importText(DEBUG)
+
+    dataset = getDataset(text, idx2char, text_as_int, DEBUG=DEBUG)
+    # Length of the vocabulary in chars
+    vocab_size = len(vocab)
+    model = build_model(
+        vocab_size=len(vocab))
+
+    if DEBUG:
+        for input_example_batch, target_example_batch in dataset.take(1):
+            example_batch_predictions = model(input_example_batch)
+            print(example_batch_predictions.shape,
+                  "# (batch_size, sequence_length, vocab_size)")
+
+        sampled_indices = tf.random.categorical(
+            example_batch_predictions[0], num_samples=1)
+        sampled_indices = tf.squeeze(sampled_indices, axis=-1).numpy()
+
+        print(sampled_indices)
+
+        print("Input: \n", repr("".join(idx2char[input_example_batch[0]])))
+        print()
+        print("Next Char Predictions: \n", repr(
+            "".join(idx2char[sampled_indices])))
+
+        example_batch_loss = loss(
+            target_example_batch, example_batch_predictions)
+        print("Prediction shape: ", example_batch_predictions.shape,
+              " # (batch_size, sequence_length, vocab_size)")
+        print("scalar_loss:      ", example_batch_loss.numpy().mean())
+
+    model.compile(optimizer='adam', loss=loss)
+
+    # Directory where the checkpoints will be saved
+    checkpoint_dir = './training_checkpoints'
+    # Name of the checkpoint files
+    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_prefix,
+        save_weights_only=True)
+
+    EPOCHS = 10
+    history = model.fit(dataset, epochs=EPOCHS,
+                        callbacks=[checkpoint_callback])
+
+    # tf.train.latest_checkpoint(checkpoint_dir)
+    # model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
+
+    # model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+
+    # model.build(tf.TensorShape([1, None]))
+
+    model.summary()
+
+    print(generate_text(model, start_string=u"ROMEO: "))
+
+
+if __name__ == "__main__":
+    main()
