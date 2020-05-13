e9f7f1dc1e1cc09829ec6f48532810629dbe5925
Caio <caiofcastro@usp.br>
2020-05-13 18:33:21 -0300

    rename + add file to just load model

--- shakespeare/train.py
+++ shakespeare/train.py
@@ -123,45 +123,6 @@ def loss(labels, logits):
     return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
 
 
-def generate_text(model, start_string):
-    # Evaluation step (generating text using the learned model)
-
-    # Number of characters to generate
-    num_generate = 1000
-
-    # Converting our start string to numbers (vectorizing)
-    input_eval = [char2idx[s] for s in start_string]
-    input_eval = tf.expand_dims(input_eval, 0)
-
-    # Empty string to store our results
-    text_generated = []
-
-    # Low temperatures results in more predictable text.
-    # Higher temperatures results in more surprising text.
-    # Experiment to find the best setting.
-    temperature = 1.0
-
-    # Here batch size == 1
-    model.reset_states()
-    for i in range(num_generate):
-        predictions = model(input_eval)
-        # remove the batch dimension
-        predictions = tf.squeeze(predictions, 0)
-
-        # using a categorical distribution to predict the character returned by the model
-        predictions = predictions / temperature
-        predicted_id = tf.random.categorical(
-            predictions, num_samples=1)[-1, 0].numpy()
-
-        # We pass the predicted character as the next input to the model
-        # along with the previous hidden state
-        input_eval = tf.expand_dims([predicted_id], 0)
-
-        text_generated.append(idx2char[predicted_id])
-
-    return (start_string + ''.join(text_generated))
-
-
 def main():
     text, vocab, char2idx, idx2char, text_as_int = importText(DEBUG)
 
@@ -206,19 +167,8 @@ def main():
         save_weights_only=True)
 
     EPOCHS = 10
-    history = model.fit(dataset, epochs=EPOCHS,
+    history = model.fit(dataset, epochs=10,
                         callbacks=[checkpoint_callback])
-
-    # tf.train.latest_checkpoint(checkpoint_dir)
-    # model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
-
-    # model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
-
-    # model.build(tf.TensorShape([1, None]))
-
-    model.summary()
-
-    print(generate_text(model, start_string=u"ROMEO: "))
 
 
 if __name__ == "__main__":
